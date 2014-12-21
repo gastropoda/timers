1598cd5f4a0686b255e6d911b7e35f7f92f3a476
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-21 09:39:48 +0100

    sanitizing the files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,2 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    bower: {
-      target: {
-        rjsConfig: 'js/config.js',
-        options: {
-          transitive: true
-        }
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-bower-requirejs');
 };
