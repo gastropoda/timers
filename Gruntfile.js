02e3092277890ab84ce0ddaf33ef28bfdfc568bb
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-21 18:45:09 +0100

    added download-atom-shell grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,2 +1,9 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    'download-atom-shell': {
+      version: '0.20.1',
+      outputDir: 'dist'
+    }
+  });
+  grunt.loadNpmTasks("grunt-download-atom-shell");
 };
