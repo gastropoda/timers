854b430bc66fd82906875b9a2838675cc89e14cc
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-22 09:37:16 +0100

    custom formats in the time filter

--- js/timers.js
+++ js/timers.js
@@ -1,4 +1,5 @@
-var minutes = 60;
+var seconds = 1000;
+var minutes = 60 * seconds;
 
 var timersApp = angular.module("timers", []);
 
@@ -6,23 +7,27 @@ timersApp.controller("TimersCtrl", function($scope) {
   $scope.elapsed = 0;
   $scope.remaining = 0;
   $scope.intervals = [
-    25*minutes,
-    5*minutes,
-    90,
-    60,
-    30,
+    25 * minutes,
+    5 * minutes,
+    90 * seconds,
+    60 * seconds,
+    30 * seconds,
   ];
 });
 
 timersApp.filter("time", function() {
   return function(input, format) {
-    var min_sec = Math.floor(input);
-    var ms = Math.round((input - min_sec)*1000);
-    var min = min_sec / 60;
+    input = parseInt(input);
+    var ms = input % seconds;
+    var min_sec = Math.floor(input / seconds);
+    var min = Math.floor(min_sec / 60);
     var sec = min_sec % 60;
 
     if (format) {
-      return input;
+      return format.
+        replace("mm", zeroPad(min), "g").
+        replace("ss", zeroPad(sec), "g").
+        replace("ms", zeroPad(ms, 3), "g");
     } else {
       if (sec) {
         return min_sec + "s";
@@ -30,5 +35,12 @@ timersApp.filter("time", function() {
         return min + "m";
       }
     }
+  };
+
+  function zeroPad(x, width) {
+    width = width || 2;
+    x = x.toString();
+    while (x.length < width) x = "0" + x;
+    return x;
   }
 });
