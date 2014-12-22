3ef37eae3ee8f580f0fe4dfa15dffab214ee8d01
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-22 09:15:29 +0100

    imagining a time filter

--- js/timers.js
+++ js/timers.js
@@ -1,13 +1,19 @@
 var timersApp = angular.module("timers", []);
 
 timersApp.controller("TimersCtrl", function($scope) {
-  $scope.elapsed = "0:00:00.000";
-  $scope.remaining = "0:00:00.000";
+  $scope.elapsed = 0;
+  $scope.remaining = 0;
   $scope.intervals = [
-    "25m",
-    "5m",
-    "90s",
-    "1m",
-    "30s",
+    25*60,
+    5*60,
+    90,
+    60,
+    30,
   ];
+});
+
+timersApp.filter("time", function() {
+  return function(input, format) {
+    return input;
+  }
 });
