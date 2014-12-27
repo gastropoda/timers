715761bcef49ae426f22b2082ecaf2d4ab34bbe9
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-12-27 18:05:54 +0100

    sound can be chosen

--- js/sound.js
+++ js/sound.js
@@ -2,8 +2,27 @@ var soundModule = angular.module("sound", []);
 
 soundModule.controller("SoundCtrl", function($scope) {
   createjs.Sound.registerSound("snd/beep.ogg", "beep");
+  createjs.Sound.registerSound("snd/aan-de-slag.ogg", "papa");
+  createjs.Sound.registerSound("snd/ye-ye-yeah.ogg", "dima");
+
+  $scope.sounds = [{
+    name: "beep",
+    label: "Bi-bi-bi-biep, bi-bi-bi-biep",
+    options: {loop: 1}
+  }, {
+    name: "dima",
+    label: "Ye-ye-yeaaaah!",
+  }, {
+    name: "papa",
+    label: "Aan de slag!",
+  }];
+  $scope.finalSound = $scope.sounds[0];
+
+  $scope.setFinal = function(sound) {
+    $scope.finalSound = sound;
+  };
 
   $scope.$on("TimerFinished", function() {
-    createjs.Sound.play("beep", {loop: 1});
+    createjs.Sound.play($scope.finalSound.name, $scope.finalSound.options);
   });
 });
